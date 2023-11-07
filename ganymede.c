dae09c312b344fb85dd9771b149ab03faefce6d5
R.K. <kawakami120604@gmail.com>
2023-11-07 11:06:02 +0900

    ver7.3

--- ganymede.c
+++ ganymede.c
@@ -25,13 +25,14 @@ int main( void ){
 	int i, j;
 	//計算の幅
 	double dt = 3.1536e+11;   //s (=10000yr)
-	double dr = 10000.0;      //m
+	double dr = 1.0e4  ;      //m
 	//計算用の変数
 	double	temp[78];
 	double newtemp[78];
 	double r[78];
 	double mass[78];
 	double g[78];
+	double l[78];
 	double f_ad;
 	double f_cmb;
 	double f_tot;
@@ -60,6 +61,7 @@ int main( void ){
 	double sigma_core = 5.0e5;    //核の電気伝導率[S/m = kg^-1 m^-3 s^3 A^2]
 	double a_core = 8.0e2;        // k/(ρ*Cp)
 	//岩石層の定数
+	double r_rock = 1.78e6;       //岩石層の表面半径[m]
 	double k_rock = 6.6;		  //岩石の熱伝導率（仮）
 	double Cp_rock = 1.0e3;       //岩石の定圧比熱[J kg^-1 K^-1]
 	double rho_rock = 3.3e3;      //岩石の密度[kg m^-3]
@@ -88,7 +90,7 @@ int main( void ){
 
 
 	//動径ステップの繰り返し
-	////FILE *file_g = fopen(filename_g, "w");
+	// FILE *file_g = fopen(filename_g, "w");
 	for(i=0; i<78; i++)	{
 		//初期条件の決定
 		temp[i] = 1100;
@@ -96,8 +98,12 @@ int main( void ){
 		r[i] = r_core + i * dr;
 		mass[i] = 4/3*M_PI * ( (rho_core-rho_rock)*pow(r_core,3.0) + rho_rock*pow(r[i],3.0) );
 		g[i] = G * mass[i] / pow(r[i],2.0);
+		if (i*dr < (r_rock-r_core)-i*dr) 
+			l[i] = i*dr;
+		else
+			l[i] = (r_rock-r_core)-i*dr;
 	}
-	////fclose(file_g);
+	// fclose(file_g);
 
 
 	//時間ステップの繰り返し
@@ -111,10 +117,10 @@ int main( void ){
         q_all = q_k + q_th + q_u235 + q_u238;
 		//境界条件を適用
 		newtemp[0] = 3 * f_cmb * dt / (Cp_rock * rho_core * r_core) + temp[0];				//内側の境界条件(∵ f_cmb(j-1) * S_c * dt = Cp_rock * rho_c * V_c * (temp[0](j-1)-temp[0](j)) )
-		temp[77] = 278.6 ;			                                                	//外側の境界条件
+		temp[77] = 278.6 ;			                                                	    //外側の境界条件
 		//温度の計算
 		for(i=1; i<77; i++)
-			newtemp[i] = dt/dr/dr*a_rock * ((1.0/(i+100)+1.0)*temp[i+1] - 2.0*temp[i] + (1.0-1.0/(i+100))*temp[i-1]) + temp[i] + q_all/Cp_rock;
+			newtemp[i] = dt/dr/dr*a_rock * ((1.0+1.0/(i+100))*temp[i+1] - 2.0*temp[i] + (1.0-1.0/(i+100))*temp[i-1]) + temp[i] + q_all/Cp_rock;
 			// newtemp[i] =  temp[i] + dt/(dr*dr) * k_rock/(Cp_rock*rho_rock) * ( (1.0+1.0/(i+100))*temp[i+1] - 2.0*temp[i] + (1.0-1.0/(i+100))*temp[i-1] ) + q_all/Cp_rock;
 		//j+1での断熱温度勾配の計算
 		f_ad = -k_core * alpha_core * newtemp[0] * g[0] / Cp_core;
@@ -146,7 +152,7 @@ int main( void ){
 	//動径分布を出力
 	FILE *file_i = fopen(filename_i, "w");
 	for (int i=0; i<78; i++) {
-		fprintf(file_i, "%d,%5.1f\n", i, temp[i]);
+		fprintf(file_i, "%d,%5.1f,%f\n", i, temp[i], l[i]);
 	}
 	fclose(file_i);
 
