4522a0e2abb0817102dce69ede3d8b062602dab7
R.K. <kawakami120604@gmail.com>
2023-11-13 05:53:51 +0900

    sub

--- ganymede.c
+++ ganymede.c
@@ -27,13 +27,15 @@ int main( void ){
 	double dt = 3.1536e+11;   //s (=10000yr)
 	double dr = 1.0e4  ;      //m
 	//計算用の変数
-	double	temp[78];
-	double newtemp[78];
+	double T_old[78];
+	double T[78];
 	double r[78];
 	double mass[78];
 	double g[78];
 	double l[78];
-	double f_ad;
+	double f_cond[78];
+	double f_conv[78];
+	double f_ad[78];
 	double f_cmb;
 	double f_tot;
 	double f_conv;
@@ -92,8 +94,6 @@ int main( void ){
 	//動径ステップの繰り返し
 	// FILE *file_g = fopen(filename_g, "w");
 	for(i=0; i<78; i++)	{
-		//初期条件の決定
-		temp[i] = 1100;
 		//重力加速度の計算
 		r[i] = r_core + i * dr;
 		mass[i] = 4/3*M_PI * ( (rho_core-rho_rock)*pow(r_core,3.0) + rho_rock*pow(r[i],3.0) );
@@ -109,42 +109,55 @@ int main( void ){
 	//時間ステップの繰り返し
 	FILE *file_j = fopen(filename_j, "w");
 	for(j = 0; j < 460001; j++){
-		//発熱量の計算
-		q_k = c_k * (h_k*dt) * exp(-log(2.0) * j / (t_half_k*100.0));
-        q_th = c_th * (h_th*dt) * exp(-log(2.0)*j/(t_half_th*100.0));
-        q_u235 = c_u235 * (h_u235*dt) * exp(-log(2.0)*j/(t_half_u235*100.0));
-        q_u238 = c_u238 * (h_u238*dt) * exp(-log(2.0)*j/(t_half_u238*100.0));
-        q_all = q_k + q_th + q_u235 + q_u238;
-		//境界条件を適用
-		newtemp[0] = 3 * f_cmb * dt / (Cp_rock * rho_core * r_core) + temp[0];				//内側の境界条件(∵ f_cmb(j-1) * S_c * dt = Cp_rock * rho_c * V_c * (temp[0](j-1)-temp[0](j)) )
-		temp[77] = 278.6 ;			                                                	    //外側の境界条件
-		//温度の計算
-		for(i=1; i<77; i++)
-			newtemp[i] = dt/dr/dr*a_rock * ((1.0+1.0/(i+100))*temp[i+1] - 2.0*temp[i] + (1.0-1.0/(i+100))*temp[i-1]) + temp[i] + q_all/Cp_rock;
-			// newtemp[i] =  temp[i] + dt/(dr*dr) * k_rock/(Cp_rock*rho_rock) * ( (1.0+1.0/(i+100))*temp[i+1] - 2.0*temp[i] + (1.0-1.0/(i+100))*temp[i-1] ) + q_all/Cp_rock;
-		//j+1での断熱温度勾配の計算
-		f_ad = -k_core * alpha_core * newtemp[0] * g[0] / Cp_core;
-		//j+1でのCMBでの熱流量の計算
-		f_cmb = k_core * (newtemp[1]-newtemp[0]) / dr;
-		//j+1でのコア内の半径rでの総熱流量
-		f_core_tot = - rho_core * Cp_core * r_core * (newtemp[0] - temp[0])/dt /3;
-		f_core_conv = f_core_tot - f_ad;
-		//j+1での平均流速を計算
-		base_ml = (r_core * f_core_conv * alpha_core * g[0]) / (rho_core * Cp_core);
-		base_mac = (f_core_conv * alpha_core * g[0]) / (rho_core * Cp_core * omega);
-		v_ml = 0.3 * pow( (r_core * f_core_conv * alpha_core * g[0]) / (rho_core * Cp_core), 1.0/3.0);
-		v_mac = pow( (f_core_conv * alpha_core * g[0]) / (rho_core * Cp_core * omega), 1.0/2.0);
-		// 磁気レイノルズ数
-		R_ml = v_ml * r_core * mu * sigma_core;
-		R_mac = v_mac * r_core * mu * sigma_core;
-		// エルザッサー数
-		lambda = sigma_core * pow(B,2) / ( 2 * rho_core * omega );
-		//温度を更新
-		for(i=0; i<77; i++)
-			temp[i] = newtemp[i];
+		
+		if(j==0){
+			//初期条件の設定
+			T_old[i] = 1100;
+			T[i] = 1100;
+		}
+
+		else{
+			//発熱量の計算
+			q_k = c_k * (h_k*dt) * exp(-log(2.0) * j / (t_half_k*100.0));
+			q_th = c_th * (h_th*dt) * exp(-log(2.0)*j/(t_half_th*100.0));
+			q_u235 = c_u235 * (h_u235*dt) * exp(-log(2.0)*j/(t_half_u235*100.0));
+			q_u238 = c_u238 * (h_u238*dt) * exp(-log(2.0)*j/(t_half_u238*100.0));
+			q_all = q_k + q_th + q_u235 + q_u238;
+
+			//断熱温度勾配の計算
+			f_ad = -k_core * alpha_core * T[0] * g[0] / Cp_core;
+			//CMBでの熱流量の計算
+			f_cmb = k_core * (T[1]-T[0]) / dr;
+			//コア内の半径rでの総熱流量
+			f_core_tot = - rho_core * Cp_core * r_core * (T[0] - T_old[0])/dt /3;
+			f_core_conv = f_core_tot - f_ad[0];
+			//平均流速を計算
+			v_ml = 0.3 * pow( (r_core * f_core_conv * alpha_core * g[0])/(rho_core * Cp_core), 1.0/3.0);
+			v_mac = pow( (f_core_conv * alpha_core * g[0]) / (rho_core * Cp_core * omega), 1.0/2.0);
+			// 磁気レイノルズ数
+			R_ml = v_ml * r_core * mu * sigma_core;
+			R_mac = v_mac * r_core * mu * sigma_core;
+			// エルザッサー数
+			lambda = sigma_core * pow(B,2.0) / ( 2.0 * rho_core * omega );
+			
+			//境界条件を適用
+			T[0] = 3 * f_cmb * dt / (Cp_rock * rho_core * r_core) + T_old[0];				//内側の境界条件(∵ f_cmb(j-1) * S_c * dt = Cp_rock * rho_c * V_c * (T_old[0](j-1)-T_old[0](j)) )
+			T_old[77] = 278.6 ;			                                                	    //外側の境界条件
+			//温度の計算
+			for(i=1; i<77; i++)	{
+				f_ad[i] = 
+				T[i] = dt/dr/dr*a_rock * ((1.0+1.0/(i+100))*T_old[i+1] - 2.0*T_old[i] + (1.0-1.0/(i+100))*T_old[i-1]) + T_old[i] + q_all/Cp_rock;
+			
+			//温度を更新
+			for(i=0; i<77; i++)
+				T_old[i] = T[i];
+			}
+		
+		}
+
 		//時間変化を出力
-		printf("%d,%f,%f,%f,%f,%f,%f\n", j, temp[0], f_core_tot, f_core_conv, R_ml, R_mac, lambda);
-		fprintf(file_j, "%d,%f,%f,%f,%f,%f,%f\n", j, temp[0], f_ad, f_cmb, R_ml, R_mac, lambda); 
+		printf("%d,%f,%f,%f,%f,%f,%f\n", j, T_old[0], f_core_tot, f_core_conv, R_ml, R_mac, lambda);
+		fprintf(file_j, "%d,%f,%f,%f,%f,%f,%f\n", j, T_old[0], f_ad[0], f_cmb, R_ml, R_mac, lambda); 
 	}
 	fclose(file_j);
 
@@ -152,7 +165,7 @@ int main( void ){
 	//動径分布を出力
 	FILE *file_i = fopen(filename_i, "w");
 	for (int i=0; i<78; i++) {
-		fprintf(file_i, "%d,%5.1f,%f\n", i, temp[i], l[i]);
+		fprintf(file_i, "%d,%5.1f,%f\n", i, T_old[i], l[i]);
 	}
 	fclose(file_i);
 
